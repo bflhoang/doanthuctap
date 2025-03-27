58d108586550436ee9997438489ab9c7e5c48ee6
dumajdz <nguyenduc652003@email.com>
2025-03-27 19:38:36 +0700

    8-11

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BeeComputer.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BeeComputer.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly DataContext _dataContext;
+        public ProductController(DataContext context)
+        {
+            _dataContext = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Details()
+        public IActionResult Detail(int Id)
         {
-            return View();
+            if (Id == null) return RedirectToAction("Index");
+
+            var productsById = _dataContext.Products.Where(p=>p.Id==Id).FirstOrDefault(p => p.Id == Id);
+            return View(productsById);
         }
     }
 }
